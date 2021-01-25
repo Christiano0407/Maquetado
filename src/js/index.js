d4ddb14fc88df052760f8d6e56b41bc67c143c18
Christiano0407 <placebo0704@gmail.com>
2021-01-25 09:21:20 -0600

    menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const mainNav = document.querySelector(`.main-nav`);
-const navList = document.querySelector(`.nav-list`);
+/* const navList = document.querySelector(`.nav-list`); */
 const hamburgerMenu = document.querySelector(`.hamburger-menu`);
 
 hamburgerMenu.addEventListener(`click`, function() {
