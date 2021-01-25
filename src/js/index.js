cdc511289a88fd2ff3e27e9b1296e7ea7c5a5706
Christiano0407 <placebo0704@gmail.com>
2021-01-25 08:48:13 -0600

    nav-animation

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const mainNav = document.querySelector(`.main-nav`);
+const navList = document.querySelector(`.nav-list`);
+const hamburgerMenu = document.querySelector(`.hamburger-menu`);
+
+hamburgerMenu.addEventListener(`click`, function() {
+    mainNav.classList.toggle(`open`);
+})
